d90e0478807a426d69b54c99a6bd44a1a82ff75b
Arihant Jain <aj148@duke.edu>
2015-02-09 19:27:31 -0500

    threads_init start

--- deli.cc
+++ deli.cc
@@ -2,8 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
 int CORKBOARD_MAXCAPACITY;
 int NUM_CASHIERS;
+
+void master_maker(void* args);
+void cashier(void* args);
+
 int main(int argc, char *argv[]){
 	int count; 
 	
@@ -14,18 +19,19 @@ int main(int argc, char *argv[]){
 		}
 	}
 	NUM_CASHIERS=count-2; //Number of input files aka number of cashiers (one input file per cashier)
-	CORKBOARD_MAXCAPACITY = (*argv[1])-'0'; //hackey way to convert character string to integer
+	CORKBOARD_MAXCAPACITY = argc; //(*argv[1])-'0'; //hackey way to convert character string to integer
 	printf("There are %d cashiers\n", NUM_CASHIERS);
 	printf("The corkboard capacity is %d\n", CORKBOARD_MAXCAPACITY);
 
-	//dthreads_init((dthreads_func_t) master_maker, 0);
+	dthreads_init((dthreads_func_t) master_maker, (void*)argv);
 }
-void master_maker(){
+void master_maker(void* args){
 	printf("master_maker thread started");
-	// for(int i=0;i<NUM_CASHIERS;i++){
-	// 	dthreads_start(dthreads_func_t func, void *arg);
-	// }
+	for(int i=0;i<NUM_CASHIERS;i++){
+		dthreads_start(dthreads_func_t cashier, (void *)(args+(4*i)));
+
+	}
 }
-void cashier(){
+void cashier(void* args){
 
 }
