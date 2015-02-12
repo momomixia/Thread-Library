2cc88bc792d637e60d68256ff630189f1ffe5f6b
Nick Balkissoon <nickbalkissoon@Nicks-MacBook-Pro.local>
2015-02-12 17:40:10 -0500

    shit this library is HARD

--- 1t.cc
+++ 1t.cc
@@ -9,23 +9,38 @@
 using namespace std;
 
 extern int dthreads_init(dthreads_func_t func, void * arg){
-	printf("builing new thread\n");
-	ucontext_t * ucontext_ptr; 
-	getcontext(ucontext_ptr); // ucontext_ptr has type (ucontext_t *)
-    /*
-     * Direct the new thread to use a different stack. Your thread
-     * library should allocate STACK_SIZE bytes for each thread's
-     * stack.
-     */
+	printf("initializing new thread\n");
+	ucontext_t ucontext_ptr; 
+	getcontext(&ucontext_ptr); 
+
     char *stack = new char [STACK_SIZE];
-    ucontext_ptr->uc_stack.ss_sp = stack;
-    ucontext_ptr->uc_stack.ss_size = STACK_SIZE;
-    ucontext_ptr->uc_stack.ss_flags = 0;
-    ucontext_ptr->uc_link = NULL;
-    /*
-     * Direct the new thread to start by calling start(arg1, arg2).
-     */
-    makecontext(ucontext_ptr, (void (*)()) func, 1, arg);
+    ucontext_ptr.uc_stack.ss_sp = stack;
+    ucontext_ptr.uc_stack.ss_size = STACK_SIZE;
+    ucontext_ptr.uc_stack.ss_flags = 0;
+    ucontext_ptr.uc_link = NULL;
+
+    makecontext(&ucontext_ptr, (void (*)()) func, 1, arg);
+    setcontext(&ucontext_ptr);
+    printf("finishing thread\n");
 }
-// int dthreads_start(dthreads_func_t func, void *arg);
-// int dthreads_yield(void);
+extern int dthreads_start(dthreads_func_t func, void *arg){
+	printf("starting new thread\n");
+	ucontext_t new_context; 
+	ucontext_t current_context;
+	getcontext(&new_context); 
+	getcontext(&current_context);
+	
+    char *stack = new char [STACK_SIZE];
+    new_context.uc_stack.ss_sp = stack;
+    new_context.uc_stack.ss_size = STACK_SIZE;
+    new_context.uc_stack.ss_flags = 0;
+    new_context.uc_link = NULL;
+    makecontext(&new_context, (void (*)()) func, 1, arg);
+
+    
+    swapcontext(&current_context, &new_context);
+    //setcontext(&new_context);
+}
+extern int dthreads_yield(void){
+
+}
--- test_basic.cc
+++ test_basic.cc
@@ -10,12 +10,20 @@
 #include <iostream>
 using namespace std;
 void master_thread(void* args);
+void child_thread(void* args);
 
 int main(int argc, char *argv[]){
 	int magic_number = 7777;
 	dthreads_init((dthreads_func_t) master_thread, &magic_number);
+	printf("danger!!");
 }
 void master_thread(void * args){
 	printf("master thread started\n");
 	printf("magic number is %d\n", * (int *) args);
+	dthreads_start((dthreads_func_t) child_thread, args);
+	printf("is this shit still running???\n");
+}
+void child_thread(void * args){
+	printf("child thread running\n");
+	return;
 }
